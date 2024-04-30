1ca55959be24e37e9c68bfe8c10a5599f5a377c6
admin <admin@DESKTOP-MTHII89>
2024-04-30 09:18:29 +0900

    f

--- myhome/src/main/webapp/js/data.js
+++ myhome/src/main/webapp/js/data.js
@@ -1,17 +1,17 @@
-/**
- * data.js
- */
+/*
+* data.js
+*/
 const members = [
-	{memNum: 1001, memName: '홍길동', memPoint: 90},
-	{memNum: 1002, memName: '김길동', memPoint: 100},
-	{memNum: 1003, memName: '박길동', memPoint: 95}
+	{ memNo: 1001, memName: '홍길동', memPnt: 90 },
+	{ memNo: 1002, memName: '김길동', memPnt: 100 },
+	{ memNo: 1003, memName: '박길동', memPnt: 95 }
 ];
 
 // 배열 for.
-//for(let mem of members){}
-	
-members.forEach(function(item, idx, ary){
-	if(idx.memPoint >= 95)
+//for(let mem of members) {}
+
+members.forEach(function(item, idx, ary) {
+	if (item.memPnt >= 95)
 		console.log(item, idx, ary)
 })
 
